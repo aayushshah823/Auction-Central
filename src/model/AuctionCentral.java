c763d8d492ef6d79a58bf68fb344ce7f7ad5be3e
Allen Whitemarsh <awhitema@uw.edu>
2018-05-04 14:57:22 -0700

    Auction Central Class Done, No comments

--- src/model/AuctionCentral.java
+++ src/model/AuctionCentral.java
@@ -1,12 +1,16 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class AuctionCentral {
-
+public class AuctionCentral implements java.io.Serializable{
 	
-	
-	//private Map<NonProfit, ArrayList<Auction>> myOrganization;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3851687924011616060L;
+	private Map<NonProfit, ArrayList<Auction>> myOrganization;
 	//Username / name of the person
 	private ArrayList<NonProfit> nonProfits;
 	
@@ -16,16 +20,26 @@ public class AuctionCentral {
 	private int numCurrentAuctions;
 	
 	public AuctionCentral() {
+		myOrganization = new HashMap<>();
+		nonProfits = new ArrayList<>();
 		numCurrentAuctions = 0;
 	}
 	
-	public void addNonprofit() {
+	public void addNonprofit(NonProfit theNonProfit) {
 		//If the nonprofit is not in the system yet
 		//add a new key value and the name of the auction if any
 		// "None" if not auction is booked yet
+		if (!myOrganization.containsKey(theNonProfit)) {
+			myOrganization.put(theNonProfit, new ArrayList<Auction>());
+			nonProfits.add(theNonProfit);
+		}
 	}
 	
-	public void addAuction() {
-		
+	public void addAuction(NonProfit theNonProfit, Auction theAuction) {
+		if (theNonProfit != null && theAuction != null
+				&& numCurrentAuctions < MAX_NUM_UPCOMING_AUCTIONS) {
+			myOrganization.get(theNonProfit).add(theAuction);
+			numCurrentAuctions++;
+		}
 	}
 }
