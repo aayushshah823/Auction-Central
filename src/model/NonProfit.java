6e060e958c924010e615ba777bdd7b427391d70e
Aayush <aayushshah823@yahoo.com>
2018-05-05 11:48:11 -0700

    Added java doc comments & base cases.

--- src/model/NonProfit.java
+++ src/model/NonProfit.java
@@ -6,19 +6,40 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author Aayush, Allen, Ben, Raisa
+ * @version 5/5/18
+ *
+ */
 public class NonProfit implements Serializable {
 
 
-	/** The id. */
+	/** */
 	private static final long serialVersionUID = 6004350978105195837L;
 	private static int MAX_DAYS_AWAY_FOR_AUCTION = 60;
 	private static int MIN_DAYS_AWAY_FOR_AUCTION = 14;
+	
+	/** My Organizations.*/
 	private String myOrg;
+	
+	/** Name of nonprofit org.*/
 	private String myName;
+	
+	/** List that stores all the non-profit's auction .*/
 	private List<Auction> myAuctions;
+	
+	/** Last day when non profit auctioned.*/
 	private LocalDate myLastAuctionDate;
+	
+	/** .*/
 	private int myCurrentAuction;
 
+	/**
+	 * 
+	 * @param theOrg
+	 * @param theName 
+	 */
 	public NonProfit(String theOrg, String theName) {
 		myOrg = theOrg;
 		myName = theName;
@@ -34,9 +55,10 @@ public class NonProfit implements Serializable {
 
 	/**
 
-	// Checks when the last auction was held
-	// every time a nonprofit request for auction is accepted
-	// the  lastAuctionDate must be updated 
+		Checks when the last auction was held
+		every time a non-profit request for auction is accepted
+		the  lastAuctionDate must be updated 
+
 	public boolean submitAuctionRequest(AuctionCentral auctions) {
 
 		if (myLastAuctionDate == null || (myLastAuctionDate.getYear() - (LocalDate.now().getYear()) >= 1)) {
@@ -57,7 +79,10 @@ public class NonProfit implements Serializable {
 	 * @param theAuction The auction happening.
 	 */
 	public void addAuction(Auction theAuction) {
-		myAuctions.add(theAuction);
+		if(myAuctions.isEmpty())
+			throw new IllegalArgumentException("No Non Profit auctions");
+		else
+			myAuctions.add(theAuction);
 	}
 
 	/**
@@ -70,7 +95,10 @@ public class NonProfit implements Serializable {
 		return (a >= MIN_DAYS_AWAY_FOR_AUCTION && a <= MAX_DAYS_AWAY_FOR_AUCTION);
 	}
 
-
+	/**
+	 * Get's the items for the auction
+	 * @return list that stores the items of this auction.
+	 */
 	public List<Item> getItemsInAuction() {
 		if (myCurrentAuction < 0) {
 			throw new NullPointerException("No Auctions.");
@@ -78,6 +106,10 @@ public class NonProfit implements Serializable {
 		return myAuctions.get(myCurrentAuction).getItems();
 	}
 
+	/**
+	 * Adds the item to list of my auctions.
+	 * @param theItem The name of item.
+	 */
 	public void addItem(Item theItem) {
 		if (myCurrentAuction < 0) {
 			throw new NullPointerException("No Current Auctions.");
@@ -86,21 +118,42 @@ public class NonProfit implements Serializable {
 		myAuctions.get(myCurrentAuction).addItem(theItem);
 	}
 
+	/**
+	 * 
+	 * @return The list of auctions.
+	 */
 	public List<Auction> getListOfAuction() {
-		return myAuctions;
+		if(myAuctions.size() <= 0) 
+			throw new IllegalArgumentException("No Auctions found");
+		else
+			return myAuctions;
 	}
 
+	/**
+	 * Getter for non profit.
+	 * @return The organization.
+	 */
 	public String getOrg() {
 		return myOrg;
 	}
-
+	
+	/**
+	 * Name of non- profit.
+	 * @return name associated with non-profit.
+	 */
 	public String getName() {
 		return myName;
 	}
 
+	/**
+	 * Method that keeps track of when
+	 * was last auction of Non-profit.
+	 * @return The date of last auction.
+	 */
 	public LocalDate getLastAuctionDate() {
 		return myLastAuctionDate;
 	}
+
 
 	public List<Auction> getAuctions() {
 		return myAuctions;
