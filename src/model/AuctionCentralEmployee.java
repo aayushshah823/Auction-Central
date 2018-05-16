c631697bb02d13dc2e721a452d94a5b7bc468034
Jake Yang <JLY0627@yahoo.com>
2018-05-15 22:21:31 -0700

    Add files via upload

--- src/model/AuctionCentralEmployee.java
+++ src/model/AuctionCentralEmployee.java
@@ -2,39 +2,15 @@ package model;
 
 import java.io.Serializable;
 
-public class AuctionCentralEmployee implements Serializable, User {
+public class AuctionCentralEmployee extends User implements Serializable {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8888972440798471246L;
-	private static final String USER_TYPE = "employee";
-	private String userName;
-	private String name;
 	
-	@Override
-	public void setUsername(String userName) {
-		this.userName = userName;
-	}
-
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String getUsername() {
-		return userName;
-	}
-
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String getUserType() {
-		return USER_TYPE;
+	public AuctionCentralEmployee(String theUserName, String theName) {
+		super(theUserName, theName, "employee");
 	}
 	
 	public void updateAuctionLimit(AuctionCentral ac, int max) {
