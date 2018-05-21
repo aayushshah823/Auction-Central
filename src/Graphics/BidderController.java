010493267a1ac6e670840c3c78a4190d0755cbf7
Jake Yang <JLY0627@yahoo.com>
2018-05-20 23:07:02 -0700

    Add files via upload

--- src/Graphics/BidderController.java
+++ src/Graphics/BidderController.java
@@ -4,6 +4,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import model.AuctionCentral;
 /**
  * 
  * @author Raisa
@@ -11,10 +14,22 @@ import javafx.fxml.Initializable;
  */
 public class BidderController implements Initializable {
 
+	private AuctionCentral ac;
+	
+	@FXML
+	Label name;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+	}
+	
+	public void setAuctionCentral(AuctionCentral ac) {
+		this.ac = ac;
+	}
+
+	public void setName(String theText) {
+		name.setText(theText);
 	}
 
 }
--- src/Graphics/LoginController.java
+++ src/Graphics/LoginController.java
@@ -5,12 +5,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.AuctionCentral;
 /**
  * 
  * @author Raisa
@@ -26,23 +30,48 @@ public class LoginController implements Initializable{
 	 * @throws IOException 
 	 */
 	
+	@FXML
+	Button button;
+	
+	private AuctionCentral ac;
+	
 	public void login(ActionEvent event) throws IOException {
-		Parent bidderParent = FXMLLoader.load(getClass()
-				.getResource("/Graphics/Bidder.fxml"));
-		Scene bidderScene = new Scene(bidderParent);
-		
-		//This line gets the stage information
-		//We need to have a way to connect this with our model to get 
-		//the user information.
-		Stage bidderWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
-		bidderWindow.setScene(bidderScene);
-		bidderWindow.show();
+//		Parent bidderParent = FXMLLoader.load(getClass()
+//				.getResource("/Graphics/Bidder.fxml"));
+//		Scene bidderScene = new Scene(bidderParent);
+//		
+//		//This line gets the stage information
+//		//We need to have a way to connect this with our model to get 
+//		//the user information.
+//		Stage bidderWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+//		bidderWindow.setScene(bidderScene);
+//		bidderWindow.show();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Graphics/Bidder.fxml"));
+        AnchorPane anchorPane = loader.load();
+        Stage bidderWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(anchorPane);
+        bidderWindow.setScene(scene);
+        
+        BidderController controller = (BidderController) loader.getController();
+        controller.setAuctionCentral(ac);
+        controller.setName("Welcome " + ac.getAuctionsSortedByDate().get(4).getAuctionName());
+        
+        bidderWindow.show();
+        
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
 	}
+	
+	public void setAuctionCentral(AuctionCentral ac) {
+		this.ac = ac;
+	}
+
+	public void setButtonText(String theText) {
+		button.setText(theText);
+	}
+	
 
 }
