16662ee73aa8d8bc347bed861b858aa64603b403
Aayush <aayushshah823@yahoo.com>
2018-05-20 22:44:04 -0700

    Non Profit Controller

---
+++ src/Graphics/NonProfitController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Graphics;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * @author aayushshah
+ *
+ */
+public class NonProfitController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
