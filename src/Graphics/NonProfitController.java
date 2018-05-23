59021c6da0060bde918f729156d6e8c2da0e45c4
Ben Yuen <benny08y@gmail.com>
2018-05-23 00:54:06 -0700

    Added controller to nonprofitWelcoome fxml. Fix auctReq GUI

--- src/Graphics/NonProfitController.java
+++ src/Graphics/NonProfitController.java
@@ -3,12 +3,23 @@
  */
 package Graphics;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.converter.LocalDateStringConverter;
 import model.AuctionCentral;
 import model.NonProfit;
 
@@ -22,6 +33,22 @@ public class NonProfitController implements Initializable {
 	@FXML
 	Button button2;
 	
+	@FXML
+	private TextField startDateTxtField;
+	@FXML
+	private TextField startTimeTxtField;
+	@FXML
+	private TextField endTimeTxtField;
+	@FXML
+	private Label startDateLabel;
+	@FXML
+	private Label startTimeLabel;
+	@FXML
+	private Label endTimeLabel;
+	@FXML
+	private Button submitButton;
+	
+	
 	private AuctionCentral myAuctionCentral;
 	private NonProfit myNonProfit;
 	
@@ -34,6 +61,23 @@ public class NonProfitController implements Initializable {
 	public void construct(AuctionCentral ac, NonProfit nonProfit) {
 		this.myAuctionCentral = ac;
 		this.myNonProfit = nonProfit;
+	}
+	
+	@FXML
+	public void viewAuctionRequestScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Graphics/nonprofitAuctionRequest.fxml"));
+        AnchorPane anchorPane = loader.load();
+        Stage auctionRequest = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(anchorPane);
+        auctionRequest.setScene(scene);
+        auctionRequest.show();
+	}
+	
+	@FXML
+	public void submit(ActionEvent event) throws IOException {
+		//LocalDate inputDate = LocalDate.of(year,month,dayOfMonth);
+//		LocalDate startDate = LocalDate.of(year,month,dayOfMonth);
+//		myAuctionCentral.auctionRequest(myNonProfit, startDate, startTime, duration, theName);
 	}
 
 
